aeef42e303e27a75a91bc9f8700dae1163fcf85d
Rima <rimameih@gmail.com>
2023-05-11 06:54:48 +0700

    fix: change routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,4 @@ import employee from './employee.route'
 
 const api = Router().use('/auth', auth).use(department).use(employee)
 
-export default Router().use('/v1', api)
+export default Router().use('/api/v1', api)
